c8032df264df95ee2a1d6232d56dc18f4f8b3af3
abelardob <abelardobeltran19@gmail.com>
2025-06-07 02:21:49 -0700

    proof

--- functions/api/[[path]].js
+++ functions/api/[[path]].js
@@ -122,7 +122,6 @@ async function handleReseed(request, env) {
         ('MPL', 'Surveillance Apr 2023', 131189.04, 'Outstanding', '2023-04-30'),
         ('MPL', 'Surveillance May 2023', 131189.04, 'Outstanding', '2023-05-31'),
         ('MPL', 'Surveillance Jun 2023', 131189.04, 'Outstanding', '2023-06-30'),
-        ('AMBIDEST, S.A. DE C.V.', 'Environmental services MIA-P', 127600.00, 'Outstanding', '2023-03-20');
     `;
     
     await env.DB.batch(reseedScript.split(';').filter(q => q.trim()).map(q => env.DB.prepare(q)));
